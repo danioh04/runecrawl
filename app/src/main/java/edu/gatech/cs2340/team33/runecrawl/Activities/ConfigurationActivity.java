1a52a6fca46a9f734ec5a0a202d468dab00e9b30
Ayush <ayushgundawar@gmail.com>
2023-09-23 18:31:14 -0400

    Set up main activity

--- app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/ConfigurationActivity.java
+++
@@ -1,6 +0,0 @@
-package edu.gatech.cs2340.team33.runecrawl.Activities;
-
-import android.app.Activity;
-
-public class ConfigurationActivity extends Activity {
-}
--- app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/MainActivity.java
@@ -1,6 +1,15 @@
 package edu.gatech.cs2340.team33.runecrawl.Activities;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+
+import edu.gatech.cs2340.team33.runecrawl.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
