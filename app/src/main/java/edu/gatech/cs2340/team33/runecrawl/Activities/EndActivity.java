e98934d5f71c296aafaf2dec5c77f5902fdb5958
Ayush <ayushgundawar@gmail.com>
2023-09-24 22:58:06 -0400

    Added comments and fixed typos

--- app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/EndActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/EndActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import edu.gatech.cs2340.team33.runecrawl.R;
-// The ending screen. Displays the leaderboard and whether the players won the game or not
-// This will be implemented in the later sprints, right now the screen is empty.
+
+/**
+ * The ending screen. Displays the leaderboard and whether the players won the game or not.
+ * This will be implemented in the later sprints, right now the screen is empty.
+ */
 public class EndActivity extends AppCompatActivity {
     /**
      * Initializes the ending screen of the game.
--- app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/GameActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/GameActivity.java
@@ -1,27 +1,23 @@
 package edu.gatech.cs2340.team33.runecrawl.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import edu.gatech.cs2340.team33.runecrawl.Objects.General.GameDifficulty;
-import edu.gatech.cs2340.team33.runecrawl.Objects.General.PlayerType;
+import androidx.appcompat.app.AppCompatActivity;
+
 import edu.gatech.cs2340.team33.runecrawl.Objects.Player;
 import edu.gatech.cs2340.team33.runecrawl.R;
 
 /**
- * This is the Game Activity Class that has the main screen that we will play on.
- * Currently the goal is to display usernmae, hp, diffciulty and the sprite picked
+ * This is the Game Activity Class that has the main screen that the user will play on.
+ * Currently the goal is to display username, HP, difficulty and the sprite picked.
  */
 public class GameActivity extends AppCompatActivity {
-    // creates an instance of player that we can use to get its attributes
-    private Player player = ConfigurationActivity.getPlayer();
+    private final Player player = ConfigurationActivity.getPlayer();
 
     /**
      * Initializes the game activity screen.
@@ -33,28 +29,29 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the layout for the game screen from XML
         setContentView(R.layout.game_screen);
-        // Make the three text views for name, difficulty level and HP that shows up in right
+
+        // Obtain references to UI components
         TextView playerName = findViewById(R.id.playerName);
         TextView difficulty = findViewById(R.id.difficulty);
         TextView hp = findViewById(R.id.hitpoints);
-        // gets the image for the sprite that will show up in top left
         ImageView spriteImage = findViewById(R.id.playerSprite);
-
-        // create the end button game to leave the game and see the end screen
         Button endButton = findViewById(R.id.endGameButton);
-        endButton.setOnClickListener((View view ) -> {
+
+        // Set up a click listener for the end game button to transition to the end activity
+        endButton.setOnClickListener((View view) -> {
             Intent nextActivity = new Intent(GameActivity.this, EndActivity.class);
             startActivity(nextActivity);
         });
-        // how the items are meant to be displayed for the user to see
+
+        // Populate UI components with player details
         playerName.setText(player.getUsername());
-        difficulty.setText((String)player.getDifficulty().toString());
-        hp.setText(Integer.toString(player.getCurrentHp()));
+        difficulty.setText(player.getDifficulty().toString());
+        hp.setText(String.valueOf(player.getCurrentHp()));
+
+        // Display the sprite image based on the player's type
         spriteImage.setImageResource(player.getType().getSpriteResId());
-
-
-
-
     }
 }
