7ac0925df5deb547c3b761cf54df1f2e39f70679
ArushVe <113310269+ArushVe@users.noreply.github.com>
2023-10-07 16:23:58 -0400

    I added the restart button that is intended to take you back to the config screen if you ended your game or lost so you can select new settings or try again

--- app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/EndActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team33/runecrawl/Activities/EndActivity.java
@@ -1,6 +1,9 @@
 package edu.gatech.cs2340.team33.runecrawl.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,5 +22,16 @@ public class EndActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.end_screen);
+
+        // Creates a button that is intended to take you back to the config screen
+        Button restartButton = findViewById(R.id.restartButton);
+
+        //// Set up a click listener for the restart button to take you back to config screen
+        restartButton.setOnClickListener((View view) -> {
+            Intent nextActivity = new Intent(this, ConfigurationActivity.class);
+            startActivity(nextActivity);
+        });
+
     }
+
 }
