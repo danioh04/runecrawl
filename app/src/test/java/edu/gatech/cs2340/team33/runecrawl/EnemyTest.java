3af9bbad0e0467ecdaeedb7465661f12079e8cec
Nazanin-Rajab <143543518+Nazanin-Rajab@users.noreply.github.com>
2023-11-12 17:35:38 -0500

    Added 4 unit tests for Enemy

--- app/src/test/java/edu/gatech/cs2340/team33/runecrawl/EnemyTest.java
+++ app/src/test/java/edu/gatech/cs2340/team33/runecrawl/EnemyTest.java
@@ -9,12 +9,38 @@ import org.junit.Test;
 
 import edu.gatech.cs2340.team33.runecrawl.Model.Enemy;
 import edu.gatech.cs2340.team33.runecrawl.Model.EnemyType;
+import edu.gatech.cs2340.team33.runecrawl.Model.GameDifficulty;
+import edu.gatech.cs2340.team33.runecrawl.Model.Player;
+import edu.gatech.cs2340.team33.runecrawl.Model.PlayerType;
+import edu.gatech.cs2340.team33.runecrawl.View.Rooms.InitialRoomActivity;
+import edu.gatech.cs2340.team33.runecrawl.ViewModel.RoomViewModel;
 
 public class EnemyTest {
     private Enemy enemy;
     @Before
     public void setUp() {
         enemy = new Enemy(EnemyType.SLIME, 100);
+        Player.initialize("testPlayer", GameDifficulty.EASY, PlayerType.MAGE);
+    }
+
+    @Test
+    public void testMoveDown() {
+        RoomViewModel room = new RoomViewModel(0, 200, 0, 200);
+        enemy.moveRandomly(room);
+        boolean result = enemy.getX() >= room.getLowerXCoordinateLimit() && enemy.getX() <= room.getUpperXCoordinateLimit()
+                && enemy.getY() <= room.getUpperYCoordinateLimit() && enemy.getY() >= room.getLowerYCoordinateLimit();
+        assertTrue(result);
+    }
+
+    @Test
+    public void testMove() {
+        float x = enemy.getX();
+        float y = enemy.getY();
+        RoomViewModel room = new RoomViewModel(0, 200, 0, 200);
+        enemy.moveRandomly(room);
+        boolean result = x != enemy.getX() || y != enemy.getY();
+        assertTrue(result);
+
     }
     @Test
     public void testEnemyAlive() {
@@ -22,23 +48,11 @@ public class EnemyTest {
         enemy.receiveDamage(damage);
         assertEquals(false, enemy.isAlive());
     }
-
     @Test
-    public void testIllegalEnemyType() {
-        assertThrows(IllegalArgumentException.class, () ->
-                new Enemy(null, 100));
+    public void testRecieveDamage() {
+        enemy = new Enemy(EnemyType.SLIME, 100);
+        int damage = 20;
+        enemy.receiveDamage(20);
+        assertEquals(80, enemy.getCurrentHp());
     }
-
-    @Test
-    public void testZeroHp() {
-        assertThrows(IllegalArgumentException.class, () ->
-                new Enemy(EnemyType.ORC, 0));
-    }
-
-    @Test
-    public void test() {
-
-    }
-
-
 }
