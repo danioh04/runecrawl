47cb1cc2f185b054be16b1dc916d947af9886beb
sanjay vijayasekar <sanjay.vijayasekar@gmail.com>
2023-11-28 21:10:14 -0500

    fixed checkstyle errors.

--- app/src/test/java/edu/gatech/cs2340/team33/runecrawl/Sprint2Tests.java
+++ app/src/test/java/edu/gatech/cs2340/team33/runecrawl/Sprint2Tests.java
@@ -3,16 +3,12 @@ package edu.gatech.cs2340.team33.runecrawl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import edu.gatech.cs2340.team33.runecrawl.Model.Enemies.EnemyType;
 import edu.gatech.cs2340.team33.runecrawl.Model.Game.Difficulty;
-import edu.gatech.cs2340.team33.runecrawl.Model.Items.BasicPotion;
-import edu.gatech.cs2340.team33.runecrawl.Model.Items.JumboPotion;
-import edu.gatech.cs2340.team33.runecrawl.Model.Items.PotionDecorator;
 import edu.gatech.cs2340.team33.runecrawl.Model.Player.Player;
 import edu.gatech.cs2340.team33.runecrawl.Model.Player.PlayerType;
 
